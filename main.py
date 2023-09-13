ab0f60c4827886beb3ca0d95d62bfb4f887c7c6d
John Castro <johhncaastro@gmail.com>
2023-09-13 08:34:56 -0500

    fix: successful write notice

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ transcript = parentDiv.find('div', class_='full-script').get_text(strip=True, se
 # print(transcript)
 
 with open('harry-potter.txt', 'w') as file:
-    file.write("Write Sussyful" + transcript)
+    file.write("Write Successful:  " + transcript)
